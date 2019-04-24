0c14ee0a2056d54d418de37826afc223b45ed90a
Blaize <me@blaize.net>
2019-04-24 14:28:05 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,14 +47,36 @@ echo "#!/bin/sh -e
 if [ ! -d  /mnt/blobfusetmp ]; then
   mkdir /mnt/blobfusetmp
 fi
+
 blobfuse /ftp/ftp-files --tmp-path=/mnt/blobfusetmp -o uid=$FTPUID -o gid=$FTPGID -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --config-file=/ftp/ftp.cfg -o allow_other --log-level=LOG_DEBUG --file-cache-timeout-in-seconds=120
 /usr/sbin/pure-ftpd /etc/pure-ftpd/pure-ftpd.conf" > /etc/rc.local
 chmod +x /etc/rc.local
 
+#Generate a self-signed certificate for the web server
+mv /etc/lighttpd/ssl/ /etc/lighttpd/ssl.$$/
+mkdir /etc/lighttpd/ssl/
+openssl req -new -x509 -keyout /etc/lighttpd/ssl/server.pem -out /etc/lighttpd/ssl/server.pem -days 9999 -nodes -subj "/C=US/ST=California/L=San Francisco/O=example.com/OU=Ops Department/CN=example.com"
+chmod 744 /etc/lighttpd/ssl/server.pem
+
+#Configure the web server with the lighttpd.conf from GitHub
+mv /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf.$$
+wget -O /etc/lighttpd/lighttpd.conf https://raw.githubusercontent.com/theonemule/simple-openvpn-server/master/lighttpd.conf
 
 echo "TLS     2
 TLSCipherSuite	HIGH
 CertFile	/ftp/ftp.pem
 PureDB	/ftp/ftp.pdb" >> /etc/pure-ftpd/pure-ftpd.conf
+
+chown -R ftpuser:ftpusers /ftp/
+find /ftp -type d -exec chmod 2750 {} \+
+find /ftp -type f -exec chmod 640 {} \+
+
+rm /var/www/html/*
+chown -R ftpuser:ftpusers /var/www/html/
+find /var/www/html/ -type d -exec chmod 2750 {} \+
+find /var/www/html/ -type f -exec chmod 640 {} \+
+wget -O /var/www/html/index.sh https://raw.githubusercontent.com/theonemule/azure-blog-storage-ftp-server/master/install.sh
+
+touch /ftp/ftp.passwd
 
 sh /etc/rc.local
